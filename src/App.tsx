9b2469999a1e9cf58249b5cb8d5270ba2c62f398
Walker Sakatch <walkersakatch@gmail.com>
2022-12-20 23:35:20 -0700

    remove stuff attempt to fix deployed site

--- src/App.tsx
+++ src/App.tsx
@@ -1,17 +1,11 @@
-import logo from './logo.svg';
 import './App.css';
-import VersionDisplay from './VersionDisplay';
 import WeaponsSection from './WeaponsSection';
 
 function App() {
 
   return (
     <div className="App">
-      {/* <header className="App-header"> */}
-        {/* <img src={logo} className="App-logo" alt="logo" /> */}
-        {/* <VersionDisplay /> */}
         <WeaponsSection />
-      {/* </header> */}
     </div>
   );
 }
