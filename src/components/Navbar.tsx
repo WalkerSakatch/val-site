8f5467612943112a49182a50494782b2f945c0dc
Walker Sakatch <walkersakatch@gmail.com>
2023-01-06 18:32:59 -0700

    fix bug where menu would be missing on desktop

--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -7,36 +7,42 @@ export default function Navbar() {
 
     const [visible, setVisible] = useState(false);
 
-    function toggleMenu() {
-        setVisible(!visible);
+    function toggleMenu(e: any) {
+        const nav = document.querySelector('.primary-navigation')!;
+        
+        //Attribute comes as a string, so converting it to boolean for ease of use
+        let visStr = nav?.getAttribute('data-visible');
+        let visBool = (visStr === 'true');
+        
+        //For some reason setAttribute won't accept !visBool.toString as a string, so I did this because I want to write as little as possible
+        let oppVisBool = !visBool
+        
+        nav.setAttribute('data-visible', oppVisBool.toString());
+        setVisible(oppVisBool);
     }
 
     return (
         <>
         <header>
             <button className='mobile-nav-toggle' aria-controls='primary-navigation' aria-expanded={false} onClick={toggleMenu}>
-                {visible 
+                { visible 
                 ? <FontAwesomeIcon icon={faXmark} size={'2xl'}/> 
                 : <FontAwesomeIcon icon={faBars} size={'2xl'}/>}
             </button>
             
             {/* {//! CONSIDER PUTTING THE UL, LI STUFF INTO IT'S OWN COMPONENT TO CLEAN THIS UP} */}
             <nav>
-                {visible
-                ?   <ul id='primary-navigation' className='primary-navigation flex' >
-                        <li>
-                            <Link to="/valorant" onClick={toggleMenu}>Home</Link>
-                        </li>
-                        <li>
-                            <Link to="/valorant/weapons" onClick={toggleMenu}>Weapons</Link>
-                        </li>
-                        <li>
-                            <Link to="/valorant/maps" onClick={toggleMenu}>Maps</Link>
-                        </li>
-                    </ul>
-
-                :   <></>
-            }
+                <ul id='primary-navigation' className='primary-navigation flex' data-visible="true">
+                    <li>
+                        <Link to="/valorant" onClick={toggleMenu}>Home</Link>
+                    </li>
+                    <li>
+                        <Link to="/valorant/weapons" onClick={toggleMenu}>Weapons</Link>
+                    </li>
+                    <li>
+                        <Link to="/valorant/maps" onClick={toggleMenu}>Maps</Link>
+                    </li>
+                </ul>
             </nav>
         </header>
         </>
