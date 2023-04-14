bde8d1ee3c3823227fdf4634a00190e9eb99513a
Walker Sakatch <walkersakatch@gmail.com>
2023-04-13 19:33:00 -0600

    login and getting region works, need to clean code

--- src/pages/LoginPage.tsx
+++ src/pages/LoginPage.tsx
@@ -2,8 +2,9 @@ import React, { useState } from 'react';
 import { Link } from 'react-router-dom'
 import axios from 'axios';
 import { AuthorizationResponse } from '@mrbabalafe/valorant-api-helper/build/api/auth/definitions/AuthorizationResponse';
-import { getEntitlementsToken, getTokenResponseFromUri } from '@mrbabalafe/valorant-api-helper';
+import { getTokenResponseFromUri, RegionToShard } from '@mrbabalafe/valorant-api-helper';
 import { AccessTokenResponse } from '@mrbabalafe/valorant-api-helper/build/api/auth/definitions/AccessTokenResponse';
+import { RegionResponse } from '@mrbabalafe/valorant-api-helper/build/api/auth/definitions/RegionResponse';
 
 export default function HomePage() {
 
@@ -12,50 +13,50 @@ export default function HomePage() {
     const [riotCode, setRiotCode] = useState('');
 
     async function handleLogin() {
-        console.log("username", username);
-        console.log("password", password);
-        let lsVersion = JSON.parse(localStorage.getItem("version")!)
-        let riotClientBuild = lsVersion.riotClientBuild;
-        let body = {riotClientBuild, username, password}
-        let loginResponse: AuthorizationResponse = await axios.post(`${process.env.REACT_APP_BASE_URL}/auth/login`, body)
+        const lsVersion = JSON.parse(localStorage.getItem("version")!);
+        const riotClientBuild = lsVersion.version.riotClientBuild;
+        const loginBody = { riotClientBuild, username, password }
+        
+        //? Make initial login request
+        const loginResponse: AuthorizationResponse = await axios.post(`${process.env.REACT_APP_BASE_URL}/auth/login`, loginBody)
             .then(res => {
                 return res.data;
             });
         
-        console.log("Login Response", loginResponse.data)
-        
-        //! Check for error
+        //? Check for error
         if(loginResponse.data.error) {
             return;
         }
 
-        //! Check for multifactor
+        //? Check for multifactor
         if(loginResponse.data.type === 'multifactor') {
-            let cookies = loginResponse.cookies;
-            let code = prompt(`CHECK ${loginResponse.data.multifactor.email} FOR A RIOT CODE:`);
-            let mfaBody = {code, riotClientBuild, cookies}
-            let mfaResponse: AuthorizationResponse = await axios.post(`${process.env.REACT_APP_BASE_URL}/auth/login2fa`, mfaBody)
+            const cookies = loginResponse.cookies;
+            const code = prompt(`CHECK ${loginResponse.data.multifactor.email} FOR A RIOT CODE:`);
+            const mfaBody = {code, riotClientBuild, cookies}
+            const mfaResponse: AuthorizationResponse = await axios.post(`${process.env.REACT_APP_BASE_URL}/auth/login2fa`, mfaBody)
                 .then(res => {
-                    console.log(res)
                     return res.data;
                 });
             
-            console.log("MFA Response", mfaResponse)
             if(mfaResponse.data.error) {
                 return;
             }
-            let tokenInfo = getTokenResponseFromUri(mfaResponse.data.response.parameters.uri);
+            const tokenInfo = getTokenResponseFromUri(mfaResponse.data.response.parameters.uri);
             sessionStorage.setItem('tokenInfo', JSON.stringify(tokenInfo));
         }
 
-        //! Otherwise should be good?
+        //? Otherwise should be good
         if(loginResponse.data.type === 'response') {
-            let tokenInfo = getTokenResponseFromUri(loginResponse.data.response.parameters.uri);
+            const tokenInfo = getTokenResponseFromUri(loginResponse.data.response.parameters.uri);
             sessionStorage.setItem('tokenInfo', JSON.stringify(tokenInfo));
         }
 
+
+
+        
         let tokenInfo: AccessTokenResponse = JSON.parse(sessionStorage.getItem("tokenInfo")!);
         let access_token = tokenInfo.access_token;
+        let id_token = tokenInfo.id_token;
         let entitlementsBody = {access_token}
         let entitlementsResponse: AuthorizationResponse = await axios.post(`${process.env.REACT_APP_BASE_URL}/auth/entitlements`, entitlementsBody)
             .then(res => {
@@ -63,8 +64,20 @@ export default function HomePage() {
             });
         
         sessionStorage.setItem('entitlements_token', entitlementsResponse.data.entitlements_token);
-        console.log("Entitlements Response", entitlementsResponse.data)
-        
+
+
+        //! THIS IS ALL REGION STUFF BELOW. PROBABLY PUT INTO IT'S OWN FUNCTION
+        let regionBody = {
+            access_token,
+            id_token
+        }
+        let regionResponse: RegionResponse = await axios.put(`${process.env.REACT_APP_BASE_URL}/auth/region`, regionBody)
+            .then(res => {
+                return res.data;
+            });
+
+        let shard = RegionToShard(regionResponse.data.affinities.live);
+        sessionStorage.setItem('shard', shard);
     }
 
     return (
